224f0c9295b2854a0e5e69718b8218b1de92fccd
古長谷 葵 <a-konagaya@newral.co.jp>
2026-06-24 13:18:46 +0900

    add:Case04.java よくある質問画面への遷移テストコード追加 WebElementを利用してログイン後、「機能」プルダウンから「ヘルプ」画面、さらに別タブで開く「よくある質問」画面へ連続して遷移するテストを作成した。別タブへの目線切り替え処理と待機処理を実装している。 期待値は以下の通り ログイン直後のタイトル：コース詳細 | LMS ヘルプ画面のタイトル：ヘルプ | LMS よくある質問画面のタイトル：よくある質問 | LMS

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,28 +38,98 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle(), "画面タイトルが一致しません");
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		WebElement loginIdField = webDriver.findElement(By.id("loginId"));
+		WebElement passwordField = webDriver.findElement(By.id("password"));
+		WebElement loginButton = webDriver.findElement(By.cssSelector("input[value='ログイン']"));
+
+		loginIdField.sendKeys("StudentAA01");
+		passwordField.sendKeys("StudentBB01");
+		loginButton.click();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle(), "コース詳細画面に遷移していません");
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// 1. まずは上部メニューの機能プルダウンをクリックして開く
+		WebElement dropdown = webDriver.findElement(By.linkText("機能"));
+		dropdown.click();
+
+		// プルダウンが開く前に次の処理に行くと失敗するため少し待機
+		try {
+			Thread.sleep(500); // 0.5秒待機
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 2. 開いたメニューの中にある「ヘルプ」リンクをクリックする
+		WebElement helpLink = webDriver.findElement(By.linkText("ヘルプ"));
+		helpLink.click();
+
+		// 画面遷移の待機
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 期待値の検証：ヘルプ画面に遷移していること
+		assertEquals("ヘルプ | LMS", webDriver.getTitle(), "ヘルプ画面に遷移していません");
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		// 1. クリックする前の「現在のタブ」のIDを記憶しておく
+		String originalWindow = webDriver.getWindowHandle();
+
+		WebElement faqLink = webDriver.findElement(By.linkText("よくある質問"));
+		faqLink.click(); // ここで別タブが開く
+
+		// 画面遷移の待機
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 2. 開いているすべてのタブを取得し、新しく開いたタブに切り替える
+		for (String windowHandle : webDriver.getWindowHandles()) {
+			if (!originalWindow.equals(windowHandle)) {
+				webDriver.switchTo().window(windowHandle);
+				break;
+			}
+		}
+
+		// 新しいタブでの期待値の検証：よくある質問画面に遷移していること
+		assertEquals("よくある質問 | LMS", webDriver.getTitle(), "よくある質問画面に遷移していません");
+		getEvidence(new Object() {
+		});
 	}
 
 }
