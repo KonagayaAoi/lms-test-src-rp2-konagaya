e1714e6046634ebad01adffe7e89fb8adedd775d
古長谷 葵 <a-konagaya@newral.co.jp>
2026-06-24 13:16:53 +0900

    add:Case03.java 受講生ログイン正常系テストコード追加 WebElementを利用して受講生のIDとパスワードを入力してログインを実行し、コース詳細画面へ正しく遷移するか確認するテストを作成した。画面遷移時の待機処理も実装している。 期待値は以下の通り タイトル：コース詳細 | LMS

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +38,40 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle(), "画面タイトルが一致しません");
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// 1. 要素の取得と入力
+		WebElement loginIdField = webDriver.findElement(By.id("loginId"));
+		WebElement passwordField = webDriver.findElement(By.id("password"));
+		WebElement loginButton = webDriver.findElement(By.cssSelector("input[value='ログイン']"));
+
+		// 初回ログイン済みの受講生ユーザーのユーザーID/PWを設定
+		loginIdField.sendKeys("StudentAA01");
+		passwordField.sendKeys("StudentBB01");
+		loginButton.click();
+
+		// 画面遷移（読み込み）が完了するまで1秒待機する
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 2. 期待値の検証：コース詳細画面に遷移していること
+		assertEquals("コース詳細 | LMS", webDriver.getTitle(), "コース詳細画面に遷移していません");
+
+		// エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
