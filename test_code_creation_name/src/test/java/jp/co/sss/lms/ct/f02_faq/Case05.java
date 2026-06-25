2818985e5b3dc55c1c1bb624bbdaf15efe974837
古長谷 葵 <a-konagaya@newral.co.jp>
2026-06-25 10:59:48 +0900

    add:Case05.java キーワード検索 正常系　テストコード追加 ログインからよくある質問画面（別タブ）まで遷移した後、検索キーワード入力と検索ボタン押下による絞り込み機能、およびクリアボタン押下による入力値の初期化機能が正常に動作するかを確認するテストを作成した。 期待値は以下の通り 検索実行後：画面内に検索キーワードが含まれること クリア実行後：検索入力欄の値が空（""）であること

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -38,41 +38,136 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8080/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle(), "画面タイトルが一致しません");
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		WebElement loginIdField = webDriver.findElement(By.id("loginId"));
+		WebElement passwordField = webDriver.findElement(By.id("password"));
+		WebElement loginButton = webDriver.findElement(By.cssSelector("input[value='ログイン']"));
+
+		loginIdField.sendKeys("StudentAA01");
+		passwordField.sendKeys("StudentBB01");
+		loginButton.click();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals("コース詳細 | LMS", webDriver.getTitle(), "コース詳細画面に遷移していません");
+		getEvidence(new Object() {
+		});
 	}
-	
+
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		WebElement dropdown = webDriver.findElement(By.linkText("機能"));
+		dropdown.click();
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		WebElement helpLink = webDriver.findElement(By.linkText("ヘルプ"));
+		helpLink.click();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals("ヘルプ | LMS", webDriver.getTitle(), "ヘルプ画面に遷移していません");
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		String originalWindow = webDriver.getWindowHandle();
+		WebElement faqLink = webDriver.findElement(By.linkText("よくある質問"));
+		faqLink.click();
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		for (String windowHandle : webDriver.getWindowHandles()) {
+			if (!originalWindow.equals(windowHandle)) {
+				webDriver.switchTo().window(windowHandle);
+				break;
+			}
+		}
+
+		assertEquals("よくある質問 | LMS", webDriver.getTitle(), "よくある質問画面に遷移していません");
+		getEvidence(new Object() {
+		});
 	}
+
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		// 1. 検索キーワード入力欄の要素の取得と入力
+		WebElement searchInput = webDriver.findElement(By.id("form"));
+		searchInput.sendKeys("キャンセル");
+
+		// 2. 検索ボタンの要素を取得
+		WebElement searchButton = webDriver.findElement(By.cssSelector("input[value='検索']"));
+		searchButton.click();
+
+		// 検索結果の画面遷移（自画面）待機
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 期待値の検証：画面に「キャンセル」という文字が含まれていること
+		String pageText = webDriver.findElement(By.tagName("body")).getText();
+		assertTrue(pageText.contains("キャンセル"), "検索結果に指定したキーワードが含まれていません");
+
+		getEvidence(new Object() {
+		});
 	}
-	
+
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
 	void test06() {
-		// TODO ここに追加
+		// 1. クリアボタンを取得してクリック
+		WebElement clearButton = webDriver.findElement(By.cssSelector("input[value='クリア']"));
+		clearButton.click();
+
+		// クリア処理待機
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 期待値の検証：検索キーワード入力欄が空欄（""）になっていること
+		WebElement searchInput = webDriver.findElement(By.id("form"));
+		assertEquals("", searchInput.getAttribute("value"), "クリアボタン押下後もキーワードが残っています");
+
+		getEvidence(new Object() {
+		});
 	}
 
 }
